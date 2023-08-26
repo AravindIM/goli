2d65c8429392c6af4612a1552d0dcaabc200a9f4
Aravind I M <aravindim@yahoo.com>
2023-08-26 13:02:47 +0530

    feat(lexer): advance function split for line, column, count

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,7 +1,12 @@
 package lexer
 
+import (
+	"errors"
+	"regexp"
+)
+
 type Position struct {
-	Index int
+	Index int64
 	Start [2]int64
 	End   [2]int64
 }
@@ -12,15 +17,34 @@ type Token struct {
 	Pos    Position
 }
 
+type Definition struct {
+	Type    string
+	Pattern string
+}
+
 type Lexer struct {
-	definitions [][]string
-	cursor      int64
+	definitions []Definition
+	count       int64
+	cursor      [2]int64
 }
 
-func (l Lexer) New(definitions [][]string) Lexer {
-	return Lexer{definitions: definitions, cursor: 0}
+func (l Lexer) New(definitions []Definition) Lexer {
+	return Lexer{
+		definitions: definitions,
+		count:       0,
+		cursor:      [2]int64{0, 0},
+	}
 }
 
-func (l Lexer) Advance(step int64) {
-	l.cursor += step
+func (l Lexer) advanceCount(step int64) {
+	l.count += step
+}
+
+func (l Lexer) advanceColumn(step int64) {
+	l.cursor[0] += step
+}
+
+func (l Lexer) advanceLine(step int64) {
+	l.cursor[0] += step
+	l.cursor[0] = 0
 }
