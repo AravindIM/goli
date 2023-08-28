b96db4d33964dda9f14f148dd1054f81284df61d
Aravind I M <aravindim@yahoo.com>
2023-08-28 17:59:54 +0530

    feat(repl): prettier prompt and messages

--- main.go
+++ main.go
@@ -18,10 +18,15 @@ func main() {
 	lex := lexer.NewLexer(definitions)
 
 	scanner := bufio.NewScanner(os.Stdin)
+	log.SetFlags(0)
+	log.SetPrefix("lexer:")
+
+	fmt.Println("Welcome to goli repl!")
+	fmt.Println("Type (exit) to exit!\n")
 
 Repl:
 	for {
-		fmt.Printf(">")
+		fmt.Print("> ")
 
 		scanner.Scan()
 		err := scanner.Err()
@@ -41,15 +46,15 @@ Repl:
 			token, err := lex.NextToken()
 			if err != nil {
 				if err.Error() == "Unmatched" {
-					log.Printf("Unmatched token at line %d and column %d\n", token.Pos.Start[0], token.Pos.Start[1])
+					log.Printf("%d:%d: Unmatched token", token.Pos.Start[0], token.Pos.Start[1])
 					break
 				}
 				if err.Error() == "Empty" {
-					log.Printf("Finished Tokenizing\n")
+					log.Printf(" Success!\n")
 					break TokenizeLine
 				}
 			}
-			log.Printf("Token: %s\n", token)
+			log.Printf("%d:%d: <%s>", token.Pos.Start[0], token.Pos.Start[1], token.Type)
 		}
 
 	}
