9f7069b52ab6cf00c9e07fd8cbf963bd41183a21
Aravind I M <aravindim@yahoo.com>
2023-08-28 14:10:42 +0530

    fix(lexer): modifier functions now passed pointers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -17,6 +17,10 @@ type Token struct {
 	Pos    Position
 }
 
+func (t Token) String() string {
+	return t.Symbol
+}
+
 type Lexer struct {
 	code        string
 	definitions [][2]string
@@ -32,24 +36,30 @@ func NewLexer(definitions [][2]string) *Lexer {
 	}
 }
 
-func (l Lexer) Analyze(code string) {
+func (l *Lexer) Analyze(code string) {
 	l.code = code
+	l.count = 0
+	l.cursor = [2]int64{0, 0}
 }
 
-func (l Lexer) advanceCount(step int64) {
+func (l Lexer) Code() string {
+	return l.code
+}
+
+func (l *Lexer) advanceCount(step int64) {
 	l.count += step
 }
 
-func (l Lexer) advanceColumn(step int64) {
+func (l *Lexer) advanceColumn(step int64) {
 	l.cursor[0] += step
 }
 
-func (l Lexer) advanceLine(step int64) {
+func (l *Lexer) advanceLine(step int64) {
 	l.cursor[0] += step
 	l.cursor[0] = 0
 }
 
-func (l Lexer) NextToken() (*Token, error) {
+func (l *Lexer) NextToken() (*Token, error) {
 	if len(l.code) == 0 {
 		return nil, errors.New("Empty")
 	}
@@ -57,6 +67,7 @@ func (l Lexer) NextToken() (*Token, error) {
 	code := []byte(l.code)
 	newLine := regexp.MustCompile(`^\n`)
 	whiteSpace := regexp.MustCompile(`^\s+`)
+	Unmatched := regexp.MustCompile(`^[^\s]+`)
 
 	for loc := newLine.FindIndex(code); loc != nil; {
 		l.advanceLine(1)
@@ -91,5 +102,19 @@ func (l Lexer) NextToken() (*Token, error) {
 		}
 	}
 
-	return nil, errors.New("Unmatched")
+	loc := Unmatched.FindIndex(code)
+	start := l.cursor
+	l.advanceCount(1)
+	l.advanceColumn(int64(loc[1] - loc[0] + 1))
+	l.code = l.code[loc[1]+1:]
+
+	return &Token{
+		Type:   "unmatched",
+		Symbol: string(code[loc[0]:loc[1]]),
+		Pos: Position{
+			Index: l.count,
+			Start: start,
+			End:   l.cursor,
+		},
+	}, errors.New("Unmatched")
 }
