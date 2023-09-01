01590ee8a11c8d054c8ac471d5b2baf08147fa06
Aravind I M <aravindim@yahoo.com>
2023-09-01 13:22:58 +0000

    feat(parser): ast nodes of type list, string and number

--- parser/parser.go
+++ parser/parser.go
@@ -1,8 +1,30 @@
 package parser
 
-import "container/list"
+type AstNode interface {
+	Type() string
+}
 
-struct Node {
-	Type string
-	Value list
+type ListNode struct {
+	Value AstNode*
+	Next AstNode*
+}
+
+func (n ListNode) Type () {
+	return "list"
+}
+
+type StringNode struct {
+	Value string
+}
+
+func (n StringNode) Type () {
+	return "string"
+}
+
+type NumberNode struct {
+	Value string
+}
+
+func (n NumberNode) Type () {
+	return "number"
 }
