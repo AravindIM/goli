7f50d7280eba76c3519bb6da60a09dc73eb27b4b
Aravind I M <aravindim@yahoo.com>
2023-09-02 22:23:40 +0530

    feat(parser): parser using stack

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,11 @@
 package parser
 
-import "gitlab.com/AravindIM/goli/lexer"
+import (
+	"container/list"
+	"log"
+
+	"gitlab.com/AravindIM/goli/lexer"
+)
 
 type AstNode interface {
 	Type() string
@@ -34,3 +39,57 @@ func (n NumberNode) Type() string {
 	return "number"
 }
 
+func Parse(lex lexer.Lexer) {
+	stack := list.New()
+	s := stack.Back()
+
+	log.SetFlags(0)
+	log.SetPrefix("goli:")
+
+TokenizeLine:
+	for {
+		token, err := lex.NextToken()
+		if err != nil {
+			if err.Error() == "Unmatched" {
+				break
+			}
+			if err.Error() == "Empty" {
+				break TokenizeLine
+			}
+		}
+		if token.Type == "start" {
+			node := &ListNode{
+				Pos: lexer.Position{
+					Start: token.Pos.Start,
+				},
+			}
+			stack.PushBack(node)
+		}
+
+		if token.Type == "string" {
+			listNode := s.Prev()
+			listNode.Value = &StringNode{
+				Value: token.Symbol,
+				Pos:   token.Pos,
+			}
+		}
+
+		if token.Type == "number" {
+			listNode := s.Prev()
+			listNode.Value = &StringNode{
+				Value: token.Symbol,
+				Pos:   token.Pos,
+			}
+		}
+
+		if token.Type == "end" {
+			s = s.Prev()
+		}
+	}
+
+	s = s.Prev()
+
+	if s != nil {
+		log.Printf("%d:%d: missing closing of list", s.Pos.Start[0], s.Pos.Start[1])
+	}
+}
