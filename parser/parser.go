d3d9c8606c9e3f94edee132337167191a3055d29
Aravind I M <aravindim@yahoo.com>
2023-09-02 21:48:42 +0530

    feat(parser): position for tnode

--- parser/parser.go
+++ parser/parser.go
@@ -1,30 +1,36 @@
 package parser
 
+import "gitlab.com/AravindIM/goli/lexer"
+
 type AstNode interface {
 	Type() string
 }
 
 type ListNode struct {
-	Value AstNode*
-	Next AstNode*
+	Value *AstNode
+	Next  *AstNode
+	Pos   lexer.Position
 }
 
-func (n ListNode) Type () {
+func (n ListNode) Type() string {
 	return "list"
 }
 
 type StringNode struct {
 	Value string
+	Pos   lexer.Position
 }
 
-func (n StringNode) Type () {
+func (n StringNode) Type() string {
 	return "string"
 }
 
 type NumberNode struct {
 	Value string
+	Pos   lexer.Position
 }
 
-func (n NumberNode) Type () {
+func (n NumberNode) Type() string {
 	return "number"
 }
+
