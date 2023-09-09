66aa2b6147365e8e3eb78936ebbc28b27afec28f
Aravind I M <aravindim@yahoo.com>
2023-09-10 00:19:41 +0530

    fix(ast): ast now stores head instead of root

--- parser/ast.go
+++ parser/ast.go
@@ -3,22 +3,17 @@ package parser
 import "errors"
 
 type Ast struct {
-	root    *AstNode
-	tail    *AstNode
-	current *AstNode
+	head *AstNode
+	tail *AstNode
 }
 
 func NewAst() *Ast {
 	return new(Ast)
 }
 
-func (a Ast) Root() *AstNode {
-	return a.root
-}
-
 func (a *Ast) appendExpression(expr *AstNode) {
-	if a.root == nil {
-		a.root = expr
+	if a.head == nil {
+		a.head = expr
 		a.tail = expr
 	} else {
 		a.tail.SetNext(expr)
@@ -27,12 +22,10 @@ func (a *Ast) appendExpression(expr *AstNode) {
 }
 
 func (a *Ast) nextExpression() (*AstNode, error) {
-	if a.current == nil {
-		a.current = a.root
-	} else if a.current.Next() != nil {
-		a.current = a.current.Next()
-	} else {
+	if a.head == nil {
 		return nil, errors.New("No new expression found")
 	}
-	return a.current, nil
+	current := a.head
+	a.head = a.head.Next()
+	return current, nil
 }
